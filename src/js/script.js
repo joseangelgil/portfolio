393012c2bf75873dac21a51c01ece8e46dec757f
joseangelgil <180407943+joseangelgil@users.noreply.github.com>
2025-05-15 10:49:21 +0200

    Update

---
+++ src/js/script.js
@@ -0,0 +1,30 @@
+fetch('../public/i18n.json')
+  .then(response => response.json())
+  .then(translations => {
+
+    function updateLanguage(language) {
+      const elements = document.querySelectorAll('[data-i18n]');
+      elements.forEach(element => {
+        const key = element.getAttribute('data-i18n');
+        if (translations[language] && translations[language][key]) {
+          element.textContent = translations[language][key];
+        }
+      });
+    }
+
+    let currentLanguage = 'en';
+    updateLanguage(currentLanguage)
+
+    document.getElementById('spanish').addEventListener('click', () => {
+      currentLanguage = 'es';
+      updateLanguage(currentLanguage)
+    })
+
+    document.getElementById('english').addEventListener('click', () => {
+      currentLanguage = 'en';
+      updateLanguage(currentLanguage)
+    })
+    
+    
+  })
+  .catch(error => console.error('Error al cargar las traducciones:', error));
