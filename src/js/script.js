097179ee749eba1c75191ad870760c0f9ce5730d
joseangelgil <180407943+joseangelgil@users.noreply.github.com>
2025-05-30 17:55:59 +0200

    Add contact links

--- src/js/script.js
+++ src/js/script.js
@@ -28,3 +28,17 @@ fetch('i18n.json')
     
   })
   .catch(error => console.error('Error al cargar las traducciones:', error));
+
+
+// Email link
+
+const user = "gilgil.joseangel";
+const domain = "gmail.com"
+const email = user + "@" + domain;
+const link = "mailto:" + email;
+
+const emailLink = document.getElementById("email-link")
+emailLink.addEventListener('click', (event) => {
+  if(!emailLink.href || emailLink.href === "#") event.preventDefault()
+  emailLink.href = link  
+})
